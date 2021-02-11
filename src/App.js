26bb7bed38170e85419d31bf17fff1986f43d04f
unknown <mishra.yash3443@gmail.com>
2021-02-12 03:03:04 +0530

    first commit web app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {useState,useEffect} from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CityInput from "./Components/CityInput"
+import CityWeather from "./Components/CityWeather"
+function App() {
+  const [city ,setcity]=useState("")
+  const [weather,setweather]=useState("")
+  const fetching=(city)=>{
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c496af33d732297c30b641b3b235e47`)
+      .then(response=>response.json())
+        .then(data=>setweather(data));
+  }
+  console.log(weather)
+  return (
+    <div className="App">
+      <CityInput city={city} setcity={setcity} fetching={fetching}/>
+      <CityWeather weather={weather}/>
+    </div>
+  );
+}
+
+export default App;
